fd2e8a9eecc1da17ecd63a5d98c255c2e66dbf33
BJ Klingenberg <bklingen@calpoly.edu>
2022-01-31 09:46:41 -0800

    update user services

---
+++ models/user-services.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const userModel = require("./user");
+
+mongoose
+  .connect("mongodb://localhost:27017/users", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((error) => console.log(error));
+
+async function getUsers(name, job) {
+  let result;
+  if (name === undefined && job === undefined) {
+    result = await userModel.find();
+  } else if (name && !job) {
+    result = await findUserByName(name);
+  } else if (job && !name) {
+    result = await findUserByJob(job);
+  }
+  return result;
+}
+
+async function findUserById(id) {
+  try {
+    return await userModel.findById(id);
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
+}
+
+async function addUser(user) {
+  try {
+    const userToAdd = new userModel(user);
+    const savedUser = await userToAdd.save();
+    return savedUser;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+async function findUserByName(name) {
+  return await userModel.find({ name: name });
+}
+
+async function findUserByJob(job) {
+  return await userModel.find({ job: job });
+}
+
+exports.getUsers = getUsers;
+exports.findUserById = findUserById;
+exports.addUser = addUser;
