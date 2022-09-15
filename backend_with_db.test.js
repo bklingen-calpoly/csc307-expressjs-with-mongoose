a84b42eb1671cba6ab166224fb64bd4ca672037f
BJ Klingenberg <bklingen@calpoly.edu>
2022-09-15 11:44:59 -0700

    fix test

--- backend_with_db.test.js
+++ backend_with_db.test.js
@@ -10,8 +10,8 @@ test("test getUsers - all", async () => {
   //   name: "Joe",
   // };
 
-  expect(result[0].name).toBe("Charlie");
-  expect(result[0].job).toBe("Janitor");
+  expect(result[0].name).toBe("Joe");
+  expect(result[0].job).toBe("Mailman");
 });
 
 test("test getUsers byName  Joe", async () => {
