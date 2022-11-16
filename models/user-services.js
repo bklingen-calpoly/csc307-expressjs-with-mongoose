c7cf71ae8a4cd1c091785811a7292ff37a06013a
BJ Klingenberg <bklingen@calpoly.edu>
2022-11-16 14:57:34 -0800

    udpates to use in memory db

--- models/user-services.js
+++ models/user-services.js
@@ -1,31 +1,48 @@
 const mongoose = require("mongoose");
-const userModel = require("./user");
+const UserSchema = require("./user");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 // Uncomment the following to debug mongoose queries, etc.
 // mongoose.set("debug", true);
+let dbConnection;
 
-mongoose.connect(
-  "mongodb+srv://" +
-    process.env.MONGO_USER +
-    ":" +
-    process.env.MONGO_PWD +
-    "@" +
-    process.env.MONGO_CLUSTER +
-    "/" +
-    process.env.MONGO_DB +
-    "?retryWrites=true&w=majority",
-  // "mongodb://localhost:27017/users",
-  {
-    useNewUrlParser: true, //useFindAndModify: false,
-    useUnifiedTopology: true,
+function setConnection(newConn) {
+  dbConnection = newConn;
+  return dbConnection;
+}
+
+function getDbConnection() {
+  if (!dbConnection) {
+    dbConnection = mongoose.createConnection(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
   }
-);
+  return dbConnection;
+}
+
+// mongoose.connect(
+//   "mongodb+srv://" +
+//     process.env.MONGO_USER +
+//     ":" +
+//     process.env.MONGO_PWD +
+//     "@" +
+//     process.env.MONGO_CLUSTER +
+//     "/" +
+//     process.env.MONGO_DB +
+//     "?retryWrites=true&w=majority",
+//   // "mongodb://localhost:27017/users",
+//   {
+//     useNewUrlParser: true, //useFindAndModify: false,
+//     useUnifiedTopology: true,
+//   }
+// );
 // .catch((error) => console.log(error));
 
 async function getUsers(name, job) {
+  const userModel = getDbConnection().model("User", UserSchema);
   let result;
   if (name === undefined && job === undefined) {
     result = await userModel.find();
@@ -40,6 +57,7 @@ async function getUsers(name, job) {
 }
 
 async function findUserById(id) {
+  const userModel = getDbConnection().model("User", UserSchema);
   // try {
   return await userModel.findById(id);
   // } catch (error) {
@@ -49,6 +67,7 @@ async function findUserById(id) {
 }
 
 async function addUser(user) {
+  const userModel = getDbConnection().model("User", UserSchema);
   // try {
   const userToAdd = new userModel(user);
   const savedUser = await userToAdd.save();
@@ -60,18 +79,26 @@ async function addUser(user) {
 }
 
 async function findUserByName(name) {
+  const userModel = getDbConnection().model("User", UserSchema);
+
   return await userModel.find({ name: name });
 }
 
 async function findUserByJob(job) {
+  const userModel = getDbConnection().model("User", UserSchema);
+
   return await userModel.find({ job: job });
 }
 
 async function findUserByNameAndJob(name, job) {
+  const userModel = getDbConnection().model("User", UserSchema);
+
   return await userModel.find({ name: name, job: job });
 }
 
 async function deleteUser(id) {
+  const userModel = getDbConnection().model("User", UserSchema);
+
   return await userModel.findByIdAndDelete(id);
 }
 
@@ -80,9 +107,17 @@ async function deleteUser(id) {
 //   await mongoose.disconnect();
 // }
 
-exports.getUsers = getUsers;
-exports.findUserById = findUserById;
-exports.findUserByName = findUserByName;
-exports.addUser = addUser;
-exports.deleteUser = deleteUser;
+// exports.getUsers = getUsers;
+// exports.findUserById = findUserById;
+// exports.findUserByName = findUserByName;
+// exports.addUser = addUser;
+// exports.deleteUser = deleteUser;
 // exports.disconnectDB = disconnectDB;
+module.exports = {
+  getUsers,
+  findUserById,
+  findUserByName,
+  addUser,
+  deleteUser,
+  setConnection,
+};
--- models/user.js
+++ models/user.js
@@ -1,21 +1,24 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  job: {
-    type: String,
-    required: true,
-    trim: true,
-    validate(value) {
-      if (value.length < 2) throw new Error("Invalid job.");
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    job: {
+      type: String,
+      required: true,
+      trim: true,
+      validate(value) {
+        if (value.length < 2) throw new Error("Invalid job.");
+      },
     },
   },
-}, {collection : 'users_list'});
+  { collection: "users_list" }
+);
 
-const User = mongoose.model("User", UserSchema);
+// const User = mongoose.model("User", UserSchema);
 
-module.exports = User;
+module.exports = UserSchema;
