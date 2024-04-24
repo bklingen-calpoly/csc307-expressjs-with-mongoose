17df554a5017ccfe853ca5cd4b823986966bbde7
BJ Klingenberg <75038922+bklingen-calpoly@users.noreply.github.com>
2024-04-24 16:29:11 -0700

    rename wf

--- backend_with_db.js
+++ backend_with_db.js
@@ -7,7 +7,7 @@ import userServices from "./models/user-services.js";
 
 const app = express();
 const port = 8000;
-const APP_VERSION = "1.0.2";
+const APP_VERSION = "1.0.3";
 
 // app.use(cors({ origin: "http://localhost" }));
 app.use(cors());
